e1e850bc00b843e815cda324e171da773a115943
Todd Sutherland <sutherlandwebdev@gmail.com>
2022-10-25 11:30:09 -0500

    App elements and global styles added, Component elements and styles added

---
+++ svelte-crash-course/src/App.svelte
@@ -0,0 +1,35 @@
+<h1>My first svelte component</h1>
+
+<p>Hello {name}</p>
+
+<div class="links">
+    <a href="https://google.com">Google</a>
+</div>
+
+<Component />
+
+<!-- Only the elements within App.svelte will be affected by these style attributes -->
+<!-- When you add styles to elements, svelte will automatically add a special svelte class to those elements -->
+<style>
+    :global(h1) {
+        color: rebeccapurple;
+        animation: zoom 5s infinite;
+    }
+
+    :global(p) {
+        font-size: 44px;
+    }
+
+    .links {
+        text-align: right;
+    }
+    @keyframes -global-zoom {
+        0%, 100% {transform: scale(3)}
+        50% {transform: scale(0.5);}
+    }
+</style>
+
+<script>
+    let name = 'Todd'
+    import Component from './Component.svelte'
+</script>
---
+++ svelte-crash-course/src/Component.svelte
@@ -0,0 +1,18 @@
+<h1>Another Component</h1>
+
+<p>Bye {name}</p>
+
+<div>
+    <a href="https://google.com">Link to google</a>
+</div>
+
+<style>
+    h1 {
+        animation: zoom 5s infinite; 
+        /* this will not work here because zoom is not defined in Component.svelte */
+    }
+</style>
+
+<script>
+    let name = 'Todd'
+</script>
